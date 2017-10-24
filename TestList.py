c605bb51374c4982a7c2b628a7d65f117565271f
Imran Ali <ali.khokhar50@gmail.com>
2017-10-24 22:08:58 +0500

    added list basics
    In this program I have used list and iterate it manually as well as through for loop

---
+++ TestList.py
@@ -0,0 +1,10 @@
+cities = ['karachi', 'hyderabad', 'Lahore']
+print("first city: "+cities[0])
+print("second city: "+cities[1])
+print("third city: "+cities[2])
+print("last city: "+cities[-1])
+print()
+
+'''looping'''
+for city in cities:
+    print(city)
