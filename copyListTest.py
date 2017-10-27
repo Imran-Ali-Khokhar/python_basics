94d983ecc24f1bfb9ebbcb4a02277fe40663a525
Imran Ali <ali.khokhar50@gmail.com>
2017-10-27 18:48:40 +0500

    copy list
    In this program copying a list is performed using : operator.

---
+++ copyListTest.py
@@ -0,0 +1,6 @@
+#Copying lists using sclice. it is correct way to copy. Donot use equal operator for copying
+fruitsILike = ['Apples', 'Bananas', 'Strawberry', 'Oranges', 'Mangoes']
+print(fruitsILike)
+
+fruitsHeLikes = fruitsILike[:]
+print(fruitsHeLikes)
