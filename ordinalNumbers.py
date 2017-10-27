7b6e695f350d9f64feee72ed4311733df84a36b3
Imran Ali <ali.khokhar50@gmail.com>
2017-10-27 18:44:55 +0500

    ordinal numbers
    In this program ordinal numbers are printed using list, for loop and if conditions.

---
+++ ordinalNumbers.py
@@ -0,0 +1,12 @@
+numbers = [num for num in range(1,10)]
+print(numbers)
+
+for number in numbers:
+    if number == 1:
+        print(str(number)+"st")
+    if number == 2:
+        print(str(number)+"nd")
+    if number == 3:
+        print(str(number)+"rd")
+    if number > 3:
+        print(str(number)+"th")
